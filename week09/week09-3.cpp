425d2d437cd58cdc6d7474244d47d63b9d1f80ef
GMLGY2VCrQ <12363051@me.mcu.edu.tw>
2025-04-18 12:05:47 +0800

    Add files via upload

---
+++ week09/week09-3.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> a(100);
+    for(int i = 0; i < 100; i++) {
+        scanf("%d ", &a[i]); 
+    }
+
+    std::sort(a.begin(), a.end());
+    
+    for(int i = 0; i < 100; i++) {
+        printf("%d ", a[i]); 
+    }
+    return 0; 
+}
+
